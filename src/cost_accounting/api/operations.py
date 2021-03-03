df4819dd3681c210b39a403f21d256081d4d0333
Ilya-Savchenko <ilya07.iv@gmail.com>
2021-03-03 23:28:56 +0300

    Added the ability to add, modify and delete expenses and income

--- src/cost_accounting/api/operations.py
+++ src/cost_accounting/api/operations.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, status
 
-from ..models.operations import Operation
+from ..models.operations import Operation, OperationKind, OperationCreate, OperationUpdate
 from ..services.operations import OperationService
 
 router = APIRouter(
@@ -11,5 +11,42 @@ router = APIRouter(
 
 
 @router.get('/', response_model=List[Operation])
-def get_operations(service: OperationService = Depends()):
-	return service.get_list()
+def get_operations(
+		kind: Optional[OperationKind] = None,
+		service: OperationService = Depends()
+):
+	return service.get_list(kind=kind)
+
+@router.post('/', response_model=Operation)
+def create_operation(
+		operation_data: OperationCreate,
+		service: OperationService = Depends()
+):
+	return service.create(operation_data)
+
+@router.get('/{operaion_id}', response_model=Operation)
+def get_operation(
+	operation_id: int,
+	service: OperationService = Depends()
+):
+	return service.get(operation_id)
+
+@router.put('/{operation_id}', response_model=Operation)
+def update_operation(
+		operation_id: int,
+		operation_data: OperationUpdate,
+		service: OperationService = Depends()
+):
+	return service.update(
+		operation_id,
+		operation_data
+	)
+
+@router.delete('/{opration_id}')
+def delete_operation(
+		operation_id: int,
+		service: OperationService = Depends()
+
+):
+	service.delete(operation_id)
+	return Response(status_code=status.HTTP_204_NO_CONTENT)
